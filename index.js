108155c099188ddaad95451ff5b846e2dac2ac5c
Diyorbek <77392009+Diyorbek0404@users.noreply.github.com>
2022-01-26 13:49:13 +0500

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,9 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({storage: storage})
-
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
 
 app.use("/api/post/", postRouter)
 app.use("/api/user/", userRouter)
